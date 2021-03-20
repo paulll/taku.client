f7e7446009ebaab65888a6685ade2e967c710095
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-03-20 02:16:55 +0200

    working tag route
    Co-Authored-By: N1kO23 <59277828+N1kO23@users.noreply.github.com>

--- api/routes/tags.js
+++ api/routes/tags.js
@@ -9,23 +9,29 @@ const fs = require("fs");
 
 const router = express.Router();
 
-router.get("/:tag", async (req, res) => {
-    const wallpapers = (await db.wallpapers.aggregate([
+router.get("/one/:tag", async (req, res) => {
+    const wallpapers = await db.wallpapers.aggregate([
         {'$match': {'tags': req.params.tag}},
         {'$sort' : {'likes': -1 }}
-    ]));
+    ]);
 
     res.status(200).json({ wallpapers });
 });
 
+// Le cursed 
 router.get("/all", async (req, res) => {
-    const tagList = (await db.wallpapers.aggregate([
-        {'addFields': {'likes': {'$size': '$likes'}} 
-        {'$project': {'tags': 1, 'likes': 1} 
-        {'$unwind': {'path': '$tags'} 
-        {'$group': {'_id': 'tags', 'tags': 
-        {'$push': {'tag': '$tags', 'likes': {'$sum': '$likes'}}}}
-    ]))
+    const tags = (await db.wallpapers.aggregate([
+        {'$addFields': {'likes': {'$size': '$likes'}}}, 
+        {'$project': {'tags': 1,  'likes': 1}}, 
+        {'$unwind': {'path': '$tags', 'preserveNullAndEmptyArrays': true }}, 
+        {'$group': {'_id': 'tags',  'tags': {'$push': {'tag': '$tags',  'likes': {'$sum': '$likes'}}}}}
+    ]))[0].tags;
+    
+    // console.log(tags.reduce(tag => tag.likes)); 
+
+    // console.log(tagList);
+    res.status(200).json({ tags });
+
 });
 
 module.exports = router 
