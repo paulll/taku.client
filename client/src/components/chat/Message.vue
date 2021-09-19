206980a69cb0a4401272d47b094fe4b5eeb14fbd
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-09-20 01:03:44 +0300

    improved text css
    Co-Authored-By: Niko <59277828+N1kO23@users.noreply.github.com>

--- client/src/components/chat/Message.vue
+++ client/src/components/chat/Message.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex gap-2 items-center">
+  <div class="flex gap-2 max-w-full items-center">
     <Avatar class="w-6 h-6" :url="state.getUser(message.author_id)?.profileImage" />
     <router-link class="username" :to="{ name: 'user', params: { uuid: state.getUser(message.author_id)?._id } }">
       <p class="text-orange-500 font-semibold">{{ state.getUser(message.author_id)?.username }}</p>
     </router-link>
-    <h1>{{ message.content }}</h1>
+    <h1 class="content">{{ message.content }}</h1>
     <p class="text-sm text-dark-500">{{ new Date(message.created_at).toLocaleTimeString() }}</p>
   </div>
 </template>
@@ -22,7 +22,10 @@ defineProps<{
 }>();
 </script>
 
-<style>
+<style scoped>
+.content {
+  word-break: break-word;
+}
 .username:hover p {
   @apply underline;
 }
