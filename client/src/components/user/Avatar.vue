71a5f094a0126271a6e7f8d4364c56bb4bcfe58a
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-09-20 05:58:56 +0300

    object cover the avatars

--- client/src/components/user/Avatar.vue
+++ client/src/components/user/Avatar.vue
@@ -1,5 +1,5 @@
 <template>
-  <img :src="url || state.defaultAvatar" alt="avatar" style="aspect-ratio: 1 / 1" class="object-contain rounded-full" />
+  <img :src="url || state.defaultAvatar" alt="avatar" style="aspect-ratio: 1 / 1" class="object-cover rounded-full" />
 </template>
 
 <script setup lang="ts">
