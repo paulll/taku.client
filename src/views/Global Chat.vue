ed81d7db02efd82f398b9d2cabb842a7a9bdc881
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-09-21 19:41:38 +0300

    replaced input with textarea

--- src/views/Global Chat.vue
+++ src/views/Global Chat.vue
@@ -4,25 +4,58 @@
       <Message class="px-2" v-for="message in messages" :key="message._id" :message="message" />
     </div>
 
-    <form
-      @submit.prevent="
-        api.sendGlobalMessage(input);
-        input = '';
-      "
-      class="bg-dark-400 p-2"
-    >
-      <input placeholder="Type here" maxlength="2000" @keypress="state.playKeystroke()" type="text" class="rounded-4px w-full bg-dark-300 outline-none border-none" v-model="input" />
-    </form>
+    <div class="bg-dark-300 p-2">
+      <textarea
+        ref="textarea"
+        placeholder="Type here"
+        style="resize: none;"
+        maxlength="2000"
+        :oninput="handleInput"
+        @keypress="handleEnter"
+        type="text"
+        class="rounded-4px w-full h-10 max-h-64 bg-dark-200 outline-none border-none"
+        v-model="input">
+      </textarea>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import { computed } from "@vue/reactivity";
-import api from "../services/api";
 import { useState } from "../services/state";
 import Message from "../components/chat/Message.vue";
+import api from "../services/api";
 const state = useState();
 const messages = computed(() => state.getGlobalMessages());
 const input = ref("");
+
+const textarea = ref<HTMLElement | undefined>(undefined);
+
+const autoResize = () => {
+  const element = textarea.value;
+  if (!element) return;
+  element.style.height = element.scrollHeight + "px";
+};
+
+const resetResize = () => {
+  const element = textarea.value;
+  if (!element) return;
+  element.style.height = "2.5rem";
+};
+
+const handleInput = () => {
+  state.playKeystroke();
+  autoResize();
+};
+
+const handleEnter = (event: KeyboardEvent) => {
+  if (event.key === 'Enter' && !event.shiftKey) {
+    api.sendGlobalMessage(input.value);
+    input.value = "";
+    event.preventDefault();
+    resetResize();
+  };
+};
+
 </script>
